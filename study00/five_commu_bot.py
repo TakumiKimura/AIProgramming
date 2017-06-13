4b746422efe41c40543a48881343bc82edd706bb
TakumiKimura <kisaragi178@gmail.com>
2017-06-13 11:35:13 +0900

    bot君のメッセージを追加できるようにした

--- study00/five_commu_bot.py
+++ study00/five_commu_bot.py
@@ -6,6 +6,13 @@
 
     def __init__(self, name = ""):
         self.name = name
+        try:
+            import json
+            d = json.load(open(name+".txt"))
+        except IOError:
+            self.messageList = self.messageList
+        else:
+            self.messageList = d
 
     def mainLoop(self):
         for i in range(5):
@@ -15,7 +22,11 @@
             if(ret_message == "さようなら。また、会いましょう。"):
                 break
         else:
-            print("今日はもう眠いです。");
+            print("今日はもう眠いです。")
+        
+        if(self.name != ""):
+            import json
+            json.dump(self.messageList, open(self.name+".txt", 'w'))
 
     def sendMessage(self, message):
         ret_var = self.nameMessageCommand(message)
@@ -43,12 +54,26 @@
     def otherMessageCommand(self, message):
         if(message == "exit" or message == "さようなら"):
             return "さようなら。また、会いましょう。"
+        elif(message[:3] == "add"):
+            message_len = len(message)
+            if(message_len > 3):
+                key_fin_index = message.find(" ", 4)
+                if(message_len > key_fin_index):
+                    self.addMessageCommand(message[4:key_fin_index], message[key_fin_index+1:])
+                    return "key:" + message[4:key_fin_index] + " value:" + message[key_fin_index+1:]
+                else:
+                    self.viewHelpMessage("add")
+            else:
+                self.viewHelpMessage("add")
         else:
             return "そのメッセージに対する返答を、私は知りません。"
 
     def addMessageCommand(self, key, value):
         self.messageList[key] = value
         
+    def viewHelpMessage(self, help):
+        if(help == "add"):
+            return "add <key> <value>"
             
 if __name__ == "__main__":
     import sys
