ab978778bb3a9bbb2d31bfad62d205bc34b7c53f
TakumiKimura <kisaragi178@gmail.com>
2017-06-13 12:00:04 +0900

    今日の授業終わり

---
+++ study01/animal.py
@@ -0,0 +1,22 @@
+﻿class Animal:
+    
+    cryMessage = ""
+
+    def cry(self):
+        print(self.cryMessage)
+
+class Cat(Animal):
+    
+    def __init__(self):
+        self.cryMessage = "にゃー"
+
+
+class Dog(Animal):
+
+    def __init__(self):
+        self.cryMessage = "わん！"
+
+animal = Cat()
+animal.cry()
+animal = Dog()
+animal.cry()
